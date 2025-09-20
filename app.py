abfee22c3eb6a81eb76e1de4df18561e332dc01e
mineproness <mineproclient@gmail.com>
2025-09-20 12:41:26 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Python + SSHX on DOM Cloud!"
+    return "Hello from Docker + Python + SSHX on DOM Cloud!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
